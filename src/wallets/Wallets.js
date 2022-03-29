a4349c488c2e20950d71c7a0fa3fbacb2f01df5c
zoz <zeroexzoz@gmail.com>
2022-03-29 21:10:09 +1100

    Add connection functionality and error handling

--- src/wallets/Wallets.js
+++ src/wallets/Wallets.js
@@ -13,14 +13,59 @@ import detectEthereumProvider from '@metamask/detect-provider'
  *
  */
 export const getMetaMask = async () => {
+  if(window.ethereum.isMetaMask){
+    const provider = window.ethereum || await detectEthereumProvider()
+    if (!provider) {
+      if(isAndroid || isIOS) {
+        const currentPath = window.location.pathname
+        window.location.replace("https://metamask.app.link/dapp/app.bprotocol.org" + currentPath)
+        return
+      }
+      EventBus.$emit("app-error", "metamask is not connected");
+      return;
+    }
+
+    const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545")
+    console.log("web3")
+    console.log(web3)
+    console.log(window.ethereum.isMetaMask)
+
+    const connectFn = async () => {
+      try{
+        const accounts = await provider.request({ method: "eth_requestAccounts" })
+        return accounts[0]
+      }
+      catch(err) {
+        if (err.code === 4001) {
+            // EIP-1193 userRejectedRequest error
+            // If this happens, the user rejected the connection request.
+            EventBus.$emit("app-error", "Please connect to Meta Mask");
+        } else {
+            EventBus.$emit("app-error", err.message);
+        }
+        throw err
+      }
+    }
+
+    return {
+      web3,
+      provider,
+      connectFn
+    }
+  }else{
+    if(window.ethereum.isTally){
+      EventBus.$emit("app-error", "Tally is currently installed and set as the default wallet");
+    }else{
+      EventBus.$emit("app-error", "Metamask is currently not installed or open");
+    }
+  }
+}
+
+export const getTally = async () => {
+  if(window.ethereum.isTally){
   const provider = window.ethereum || await detectEthereumProvider()
   if (!provider) {
-    if(isAndroid || isIOS) {
-      const currentPath = window.location.pathname
-      window.location.replace("https://metamask.app.link/dapp/app.bprotocol.org" + currentPath)
-      return
-    }
-    EventBus.$emit("app-error", "metamask is not connected");
+    EventBus.$emit("app-error", "tally is not connected");
     return;
   }
 
@@ -35,7 +80,7 @@ export const getMetaMask = async () => {
       if (err.code === 4001) {
           // EIP-1193 userRejectedRequest error
           // If this happens, the user rejected the connection request.
-          EventBus.$emit("app-error", "Please connect to Meta Mask");
+          EventBus.$emit("app-error", "Please connect to Tally");
       } else {
           EventBus.$emit("app-error", err.message);
       }
@@ -48,6 +93,9 @@ export const getMetaMask = async () => {
     provider,
     connectFn
   }
+}else{
+  EventBus.$emit("app-error", "Tally is currently not installed or is not set as the default wallet");
+}
 }
 
 export const getWalletConnect = (newConnection) => {
@@ -82,4 +130,5 @@ export const getWalletConnect = (newConnection) => {
 export const walletTypes = {
   "WALLET_CONNECT": "WALLET_CONNECT",
   "META_MASK": "META_MASK",
+  "TALLY": "TALLY"
 }
