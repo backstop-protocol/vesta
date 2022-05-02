6176d22752aed6dbb9c8f425724fded1814dc47a
shmuel <shmuel@shmuels-MacBook-Pro.local>
2022-05-02 14:12:03 +0300

    abis & config

--- src/lib/fuse/config.js
+++ src/lib/fuse/config.js
@@ -11,7 +11,7 @@ const poolsByChain = {
       collateralName: "Rari",
       description: "Rari stability pool 44",
       decimals: 18,
-      lensAddress: "0x539a3f6d1F33C77c83e9b159e23E99FD8C26e7D9",
+      bammAddress: "0x94fd843E77fe67A18d52E0AD0C9713C5a9399ef4",
       rewardAddress: "0x965772e0E9c84b6f359c8597C891108DcF1c5B1A",
       wrapperAddress: "0xEb6276Fd4D8B05104AeF4246d84D7840CB964cC8",
       masterChefAddress: "0x7Ecc7163469F37b777d7B8F45A667314030ACe24",
